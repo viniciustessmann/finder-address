a86f4fb4492b84285042ae54b95fc3818d4ffb10
Vinícius Tessmann <vinicius.tessmann@melhorenvio.com>
2022-02-07 14:59:38 -0300

    feat: start projecyt

---
+++ src/main/java/com/tessmann/postalcode/PostalcodeApplication.java
@@ -0,0 +1,13 @@
+package com.tessmann.postalcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostalcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostalcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tessmann/postalcode/PostalcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tessmann.postalcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostalcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
